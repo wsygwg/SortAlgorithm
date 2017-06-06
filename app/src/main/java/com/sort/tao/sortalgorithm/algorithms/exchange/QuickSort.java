ab79bb2859894b02746beb4c2bd6e8b3fcabf040
tao <496292631@qq.com>
2017-06-06 17:34:59 +0800

    相关链接

--- app/src/main/java/com/sort/tao/sortalgorithm/algorithms/exchange/QuickSort.java
+++ app/src/main/java/com/sort/tao/sortalgorithm/algorithms/exchange/QuickSort.java
@@ -15,4 +15,56 @@ public class QuickSort implements Sort {
     public ArrayList<Integer> sort(ArrayList<Integer> numbers, SortSend sortSend) {
         return null;
     }
+
+    private static void Fast(int[] array, int left, int right) {
+        if (left < right) {
+            int p = Partition1(array, left, right);
+            Fast(array, left, p - 1);
+            Fast(array, p + 1, right);
+        }
+//      if (left < right) {
+//          int p = Partition2(array, left, right);
+//          Fast(array, left, p - 1);
+//          Fast(array, p + 1, right);
+//      }
+    }
+
+    private static int Partition1(int[] array, int left, int right) {
+        int pivot = array[left];
+        while (left < right) {
+            while (left < right && array[right] >= pivot) {
+                right--;
+            }
+            array[left] = array[right];
+            while (left < right && array[left] <= pivot) {
+                left++;
+            }
+            array[right] = array[left];
+        }
+        array[left] = pivot;
+        return left;
+    }
+
+//    private static int Partition2(int[] array, int start, int end) {
+//        int pivot = array[start];
+//        int left = start, right = end;
+//        while (left <= right) {
+//            while (left <= right && array[left] <= pivot) {
+//                left++;
+//            }
+//            while (left <= right && array[right] >= pivot) {
+//                right--;
+//            }
+//            if (left < right) {
+//                Swap(array[right], array[left]);
+//                left++;
+//                right--;
+//            }
+//        }
+//        Swap(array[start], array[right]);
+//        return right;
+//    }
+//    static void Swap(int a,int b){
+//
+//    }
 }
